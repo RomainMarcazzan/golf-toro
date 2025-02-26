54bf7eda94117e438365b558120b0ecb0b433cc8
RomainMarcazzan <romain.marcazzan@hotmail.com>
2025-02-27 00:44:37 +0400

    Refactor weather data fetching to use NEXT_PUBLIC_URL and remove unnecessary header handling

--- app/lib/weather.ts
+++ app/lib/weather.ts
@@ -1,13 +1,8 @@
 import { WeatherData } from "@prisma/client";
-import { headers } from "next/headers";
 
 export async function getWeatherData(): Promise<WeatherData[]> {
-  const headersList = await headers();
-  const host = headersList.get("host");
-  const protocol = process.env.NODE_ENV === "development" ? "http" : "https";
-
-  const response = await fetch(`${protocol}://${host}/api/weather`, {
-    //cache: "no-store",
+  const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/weather`, {
+    cache: "no-store",
     next: { tags: ["weather"] },
   });
 
